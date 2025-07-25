d178019c40f8af0b8c2c75e8e8db9982f5c488a3
AvatarNit <143652350+AvatarNit@users.noreply.github.com>
2025-07-24 21:57:42 -0400

    Update lesson11.py

--- ML/lesson11-NLP/lesson11.py
+++ ML/lesson11-NLP/lesson11.py
@@ -30,14 +30,57 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import re # Regular Expression
 
-# Text Transfomation
+# Text Transformation
 wne = WordNetLemmatizer()
 
 def transform(data):
     corpus = []
+    for i in data:
+        newi = re.sub("[^a-zA-Z]", " ", i)
+        newi = newi.lower()
+        newi = newi.split()
+        tempList = [wne.lemmatize(word) for word in newi if word not in stopwords.words("english")]
+        corpus.append(" ".join(tempList))
+    return corpus
+
+XTrainCorpus = transform(XTrain)
+XTestCorpus = transform(XTest)
+
+print(XTrainCorpus[0:5])
     
+# Vectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 
+cv = CountVectorizer(ngram_range=(1,2))
+XTrainNew = cv.fit_transform(XTrainCorpus)
+XTestNew = cv.transform(XTestCorpus)
 
+print(XTestNew.shape , XTrainNew.shape)
 
+from sklearn.ensemble import RandomForestClassifier
+
+rfc = RandomForestClassifier(n_estimators=100)
+rfc.fit(XTrainNew,yTrain)
+
+XTrainPredict = rfc.predict(XTrainNew)
+XTestPredict = rfc.predict(XTestNew)
 
 # Sentiment Analysis
+
+# Function for new predictions
+
+def sentimentPredict(text):
+    text = transform(text)
+    text = cv.transform(text)
+    textPredict = rfc.predict(text)
+
+    if textPredict[0] == 1:
+        return "Negative"
+    else:
+        return "Positive"
+
+
+str1 = ["School is coming I am sad and unhappy"]
+str2 = ["First week of school is only 3 days I am happy, excited, relieved"]
+
+print(f"Sentiment Prediction:\n   Str1: {sentimentPredict(str1)}\n   Str2: {sentimentPredict(str2)}")
